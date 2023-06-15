77acf57eb92bb1a50e8dce7670d15b2c7e6355bf
Supersm001 <supersm00001@gmail.com>
2023-06-15 10:43:02 +0530

    commit

--- src/services/article.js
+++ src/services/article.js
@@ -2,6 +2,26 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
+// export const articleApi = createApi({
+//   reducerPath: "articleApi",
+//   baseQuery: fetchBaseQuery({
+//     baseUrl: "https://api.openai.com/v1/chat/",
+//     prepareHeaders: (headers) => {
+//       headers.set("X-RapidAPI-Key", rapidApiKey);
+//       headers.set(
+//         "X-RapidAPI-Host",
+//         "article-extractor-and-summarizer.p.rapidapi.com"
+//       );
+//       return headers;
+//     },
+//   }),
+//   endpoints: (builder) => ({
+//     getSummary: builder.query({
+//       query: (params) =>
+//         `/completions`,
+//     }),
+//   }),
+// });
 export const articleApi = createApi({
   reducerPath: "articleApi",
   baseQuery: fetchBaseQuery({
